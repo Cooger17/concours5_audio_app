558915a592005499ca9dcc8be0451bb54b769151
Cooger17 <91452422+Cooger17@users.noreply.github.com>
2023-04-02 23:59:13 +0100

    Create songs.js

---
+++ songs.js
@@ -0,0 +1,49 @@
+export const songs = [
+    {
+        title: "Stairway to Heaven",
+        artist: "Led Zeppelin",
+        audioSrc: "assets/media/stairway-to-heaven.mp3",
+        cover: "assets/cover/stairway-to-heaven.jpg",
+        duration: "08:02"
+    },
+
+    {
+        title: "Billie Jean",
+        artist: "Michael Jackson",
+        audioSrc: "assets/media/billie-jean.mp3",
+        cover: "assets/cover/billie-jean.jpg",
+        duration: "04:55"
+    },
+
+    {
+        title: "Bohemian Rhapsody",
+        artist: "Queen",
+        audioSrc: "assets/media/bohemian-rhapsody.mp3",
+        cover: "assets/cover/bohemian-rhapsody.jpg",
+        duration: "05:54"
+    },
+
+    {
+        title: "Smells Like Teen Spirit",
+        artist: "Nirvana",
+        audioSrc: "assets/media/smells-like-teen-spirit.mp3",
+        cover: "assets/cover/smells-like-teen-spirit.jpg",
+        duration: "05:01"
+    },
+
+    {
+        title: "I Will Always Love You",
+        artist: "Whitney Houston",
+        audioSrc: "assets/media/i-will-always-love-you.mp3",
+        cover: "assets/cover/i-will-always-love-you.png",
+        duration: "04:03"
+    },
+
+    {
+        title: "Imagine",
+        artist: "John Lennon",
+        audioSrc: "assets/media/imagine.mp3",
+        cover: "assets/cover/imagine.jpg",
+        duration: "03:04"
+    }
+]
